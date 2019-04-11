434864623df7b3159048b668a3c900f65eb7ef62
sunwize <molpick98@gmail.com>
2019-04-12 01:52:20 +0200

    delete

--- todolist/public/js/formulaire.js
+++
@@ -1,9 +0,0 @@
-validatePassword = () => {
-    var password = document.getElementById("password");
-    var confirm_password = document.getElementById("confirm_password");
-
-    if(password.value != confirm_password.value)
-        confirm_password.setCustomValidity("Oops ! Les mots de passe sont différents !");
-    else
-        confirm_password.setCustomValidity('');
-}
